34e1c79c22c6fd43f3972a0794c3f5c8b2793dc4
Denis Gois <denisdwtg@gmail.com>
2016-04-18 18:54:23 -0300

    Add Chrome to the dock

--- osx/dock.sh
+++ osx/dock.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 dockutil --no-restart --remove all
-dockutil --no-restart --add "$HOME/Applications/Google Chrome.app"
+dockutil --no-restart --add "/Applications/Google Chrome.app"
 dockutil --no-restart --add "/Applications/System Preferences.app"
 #dockutil --no-restart --add "$HOME/Applications/Firefox.app"
 #dockutil --no-restart --add "/Applications/Safari.app"
